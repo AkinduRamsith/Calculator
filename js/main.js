4bccee977a46fffc4478d4ba2312798f45108993
AkinduRamsith <akiyaramsith2002@gmail.com>
2023-07-26 17:13:37 +0530

    Day 1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const numbers = document.querySelectorAll('.numbers');
-const result = document.querySelector('.display span');
+const result = document.querySelector('.span');
 const signs = document.querySelectorAll('.oper'); 
 const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals'); 
 const percent = document.querySelector('.percent'); 
 const deleteN = document.querySelector('.delete'); 
+const dot = document.querySelector('.dot'); 
 
 let firstValue = "";
 let isFisrtValue = false; 
@@ -26,16 +27,16 @@ for (let i = 0; i < numbers.length; i++) {
 }
 
 function getFirstValue(el) { 
-    result.innerHTML = "";
-    firstValue += el;
-    result.innerHTML = firstValue;
+    result.value = "";
+    firstValue += el
+    result.value = firstValue;
     firstValue = +firstValue;
 }
 
 function getSecondValue(el){
     if(firstValue!= "" && sign!= ""){
         secondValue+=el;
-        result.innerHTML=secondValue;
+        result.value=secondValue;
         secondValue=+secondValue; 
     }
 }
@@ -53,7 +54,7 @@ function getSign() {
 getSign();
 
 equals.addEventListener('click',() =>{
-    result.innerHTML="";
+    result.value="";
     if(sign==="+"){
         resultValue=firstValue+secondValue;
     }else if(sign==="-"){
@@ -63,13 +64,13 @@ equals.addEventListener('click',() =>{
     }else if(sign==="/"){
         resultValue=firstValue/secondValue;
     }
-    result.innerHTML=resultValue;
+    result.value=resultValue;
     firstValue=resultValue;
     secondValue="";
 })
 
 clear.addEventListener('click',() =>{
-    result.innerHTML=0;
+    result.value=0;
     firstValue="";
     isFisrtValue=false;
     secondValue="";
@@ -80,7 +81,7 @@ clear.addEventListener('click',() =>{
 })
 
 percent.addEventListener('click',()=>{
-    result.innerHTML="";
+    result.value="";
     if(firstValue!=""){
         resultValue=firstValue/100;
         firstValue=resultValue;
@@ -88,20 +89,35 @@ percent.addEventListener('click',()=>{
     if(firstValue!="" && secondValue!="" && sign!=""){
         resultValue=resultValue/100;
     }
-    result.innerHTML=resultValue;
+    result.value=resultValue;
 })
 
 deleteN.addEventListener('click',()=>{
     if(firstValue!=""){
-        firstValue=firstValue.toString().slice(0,-1)
-        result.innerHTML=firstValue;
+        firstValue=firstValue.toString().slice(0,-1);
+        result.value=firstValue;
     }
 
     if(firstValue!="" && secondValue!="" && sign!=""){
-        secondValue=secondValue.toString().slice(0,-1)
-        result.innerHTML=secondValue;
+        secondValue=secondValue.toString().slice(0,-1);
+        result.value=secondValue;
     }
+    
     
 
 
+})
+
+dot.addEventListener('click',()=>{
+
+           
+            var lastNumber = lastNumber(result);
+            if (lastNumber.indexOf(".") === -1) {
+              
+               if (lastNumber === "") {
+                  result += "0";
+               }
+               result += dot;
+            }
+            
 })
